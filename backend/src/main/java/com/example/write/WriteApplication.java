ff44f079ea80ee72baeb968c0b5aa77c9d38f994
wnsdk <jn307742@gmail.com>
2024-09-09 21:35:52 +0900

    Fix: 테스트 환경 properties 수정

--- backend/src/main/java/com/example/write/WriteApplication.java
+++ backend/src/main/java/com/example/write/WriteApplication.java
@@ -1,13 +1,27 @@
 package com.example.write;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class WriteApplication {
 
+	@Value("${common}")
+	private String common;
+
+	@Value("${test}")
+	private String test;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WriteApplication.class, args);
 	}
 
+	@PostConstruct
+	// application.yml 파일이 무엇으로 설정되었는지 확인용
+	private void start() {
+		System.out.println("common = " + common);
+		System.out.println("test = " + test);
+	}
 }

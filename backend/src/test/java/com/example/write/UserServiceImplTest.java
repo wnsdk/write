9718b1016ca058b845f4cd292c7c086af92a8e12
wnsdk <jn307742@gmail.com>
2024-09-09 21:40:52 +0900

    Fix: 테스트 코드 애노테이션

--- backend/src/test/java/com/example/write/UserServiceImplTest.java
+++ backend/src/test/java/com/example/write/UserServiceImplTest.java
@@ -13,12 +13,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest(classes = WriteApplication.class)
 //@TestPropertySource(locations = "classpath:application-test.yml")
+@ActiveProfiles("test")
 class UserServiceImplTest {
 
     @Autowired

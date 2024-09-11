1f1d8690169b3efaf2f82cb94b69e800c0dbc3bb
wnsdk <jn307742@gmail.com>
2024-09-11 12:54:09 +0900

    Add: TestController

--- be/src/main/java/com/example/write/controller/TestController.java
+++ be/src/main/java/com/example/write/controller/TestController.java
@@ -1,12 +1,19 @@
 package com.example.write.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
 public class TestController {
 
     @GetMapping("")
-    public ResponseEntity<String> pingPong throws Exception {
+    public ResponseEntity<String> pingPong() throws Exception {
         return new ResponseEntity<>("서버와 연결이 되었습니다.", HttpStatus.CREATED);
     }
 }

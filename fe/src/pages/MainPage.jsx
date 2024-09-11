872fe12cedc6d2a1a60ebd73f48e3f47049d9c5b
wnsdk <jn307742@gmail.com>
2024-09-11 17:45:29 +0900

    Add: 메인 페이지

--- fe/src/pages/MainPage.jsx
+++ fe/src/pages/MainPage.jsx
@@ -5,8 +5,8 @@ export default function MainPage() {
 
     return (
         <>
-            <button onClick={() => navigate('/list', 'writing')}>작문하기</button>
-            <button onClick={() => navigate('/list', 'copying')}>필사하기</button>
+            <button onClick={() => navigate('/list', { state: { type: 'writing' } })}>작문하기</button>
+            <button onClick={() => navigate('/list', { state: { type: 'copying' } })}>필사하기</button>
         </>
     );
 }

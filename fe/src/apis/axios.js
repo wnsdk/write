5982bf6ec5f18d223714cabae244a900567b6840
wnsdk <jn307742@gmail.com>
2024-09-13 22:07:53 +0900

    Add: axios 설정 추가

--- fe/src/apis/axios.js
+++ fe/src/apis/axios.js
@@ -2,4 +2,7 @@ import axios from 'axios';
 
 export const $ = axios.create({
     baseURL: import.meta.env.VITE_API_BASE_URL,
+    headers: {
+        'Content-Type': 'application/json; charset=UTF-8',
+    },
 });

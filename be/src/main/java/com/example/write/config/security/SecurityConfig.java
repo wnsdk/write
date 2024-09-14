838133bf839d897dba7633ab845213de4b469ec2
wnsdk <jn307742@gmail.com>
2024-09-14 18:57:45 +0900

    Add: 운영환경에서 로그인 시도 시 액세스 토큰 검사 패스

--- be/src/main/java/com/example/write/config/security/SecurityConfig.java
+++ be/src/main/java/com/example/write/config/security/SecurityConfig.java
@@ -43,7 +43,7 @@ public class SecurityConfig {
 
         http
                 .authorizeHttpRequests((authorize) -> authorize
-                        .requestMatchers("/test/**", "/login/**", "/api/login/**").permitAll()
+                        .requestMatchers("/test/**", "/login/**", "/api/login/**", "/api/oauth2/authorization/**").permitAll()
 //                        .requestMatchers("/test/guest", "/test/login/**", "/oauth2/authorization/**").permitAll()
 //                        .requestMatchers("/test/admin").hasAuthority("ADMIN")
                         .anyRequest().authenticated() // JwtFilter 에서 authenticate 해줌
@@ -60,7 +60,7 @@ public class SecurityConfig {
                                 .authorizationEndpoint(authorizationEndpointConfig ->
                                         authorizationEndpointConfig
                                                 //Client 가 이 URI 로 요청을 보내면 OAuth 공급자에게 리디렉션 됨
-                                                .baseUri("/oauth2/authorization")
+//                                                .baseUri("/oauth2/authorization")
                                                 .baseUri("/api/oauth2/authorization")
                                                 //OAuth2 승인 요청이 자동으로 cookie 에 저장됨
                                                 .authorizationRequestRepository(oAuth2AuthorizationRequestBasedOnCookieRepository))

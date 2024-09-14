a52c24a67fcffc3ca79b340ef2632581dc653b93
wnsdk <jn307742@gmail.com>
2024-09-14 17:38:14 +0900

    Add: 로그 출력

--- be/src/main/java/com/example/write/WriteApplication.java
+++ be/src/main/java/com/example/write/WriteApplication.java
@@ -8,7 +8,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WriteApplication {
 
+	@Value("${redirect.uri}")
+	private String redirectUri;
+
 	public static void main(String[] args) {
 		SpringApplication.run(WriteApplication.class, args);
+	}
+
+	@PostConstruct
+	public void init() {
+		System.out.println("redirectUri : " + redirectUri);
 	}
 }

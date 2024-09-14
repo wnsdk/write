ecc100361a29423532f048dc410d004944bec377
wnsdk <jn307742@gmail.com>
2024-09-14 18:10:50 +0900

    Fix: 구글 로그인 redirect uri 설정 수정

--- be/src/main/java/com/example/write/WriteApplication.java
+++ be/src/main/java/com/example/write/WriteApplication.java
@@ -10,21 +10,7 @@ import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class WriteApplication {
 
-	@Value("${redirect.uri}")
-	private String redirectUri;
-
 	public static void main(String[] args) {
 		SpringApplication.run(WriteApplication.class, args);
-	}
-
-	@Bean
-	public CommandLineRunner commandLineRunner() {
-		return args -> {
-			if (redirectUri == null) {
-				System.out.println("redirectUri 값이 주입되지 않았습니다.");
-			} else {
-				System.out.println("redirectUri : " + redirectUri);
-			}
-		};
 	}
 }
